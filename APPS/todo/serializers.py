c5959069a1bb887e7acae14b3c4b90177606625b
veyyr3 <dimapolska967@gmail.com>
2025-07-19 17:31:15 +0600

    fix serializer

--- APPS/todo/serializers.py
+++ APPS/todo/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 
 from .models import Task
 
-class НазваниеSerializer(serializers.ModelSerializer):
+class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = "__all__"
